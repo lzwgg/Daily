0a87b040e1d4f0afa6f573e6be48bdec57f9ffa9
lzwgg <lzwiread@gmail.com>
2019-04-19 22:41:23 +0800

    check

---
+++ new 1.c
@@ -0,0 +1,19 @@
+static int valIndex = 0, valArrays[15][8] = {0};
+
+unsigned short servo_alarm_wave_filter(unsigned short almFlag) {
+	volatile unsigned short tempVal = almFlag;
+	unsigned short tempValArrays[8] = {0}, retVal = 0;
+	for (int i = 0; i < 8; i++) {
+		// 赋值
+		valArrays[valIndex][i] = (tempVal << i) & 1;
+	}
+	valIndex = (valIndex + 1) % 15;
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 15; j++) {
+			// 统计15次的电平变化
+			tempValArrays[i] += valArrays[j][i];
+		}
+		retVal |= (((tempValArrays[i] > 9) ? 1 : 0) << i);
+	}
+	return retVal;
+}
